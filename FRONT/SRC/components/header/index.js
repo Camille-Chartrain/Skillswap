a905889497135abc375d6a6ebb87bc0fd2bddcae
lucilebgt <lucile.bergeat@oclock.school>
2024-05-15 12:29:51 +0200

    searchBar

--- FRONT/SRC/components/header/index.js
+++ FRONT/SRC/components/header/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import BannerPc from './pictures/BannerPc.png';
 import logo from './pictures/logo.png';
+import Search from "../search/Search";
 import { ToggleBtn } from '../../util';
+
 
 //->ariana wire's icones
 import dashboard from '../../style/pictures/dashboard.svg';
@@ -11,6 +13,7 @@ import statistic from '../../style/pictures/statistic.svg';
 import message from '../../style/pictures/message.svg';
 import logout from '../../style/pictures/logout.svg';
 import search from '../../style/pictures/search.svg';
+
 
 
 
@@ -24,14 +27,8 @@ const Header = () => {
                 <img className="banner" src={BannerPc} alt='banniere du site Skillswap' />
                 <span className="arianaWire">
                     <a href="/" alt="logo du site ramenant a l'accueil" ><img className="logo" src={logo} alt='logo du site Skillswap' /></a>
-                    <div className="search">
-                        <input type="search" placeholder="rechercher" />
-                        <select>Level</select>
-                        <select>category</select>
-                        <select>sub-category</select>
-                        <button><img className="" src={search} alt=' icone de recherche' /></button>
-                    </div>
                     <div>
+
                         {/* {isLogged ? ( */}
                         <>
                             <a href="/dashboard" alt="dashboard du membre" ><img className="" src={dashboard} alt=' icone du tableau de bord ' /></a>
@@ -40,13 +37,19 @@ const Header = () => {
                             <a href="#statistic" alt=" statistic " ><img className="" src={statistic} alt='icone des statistiques ' /></a>
                             <a href="#communication" alt=" communication " ><img className="" src={message} alt='icone de communication ' /></a>
                             <a href="#logout" alt=" deconnection du site" ><img className="" src={logout} alt='icone de deconnexion' /></a>
-                            {/*
-                            */}
                         </>
-                        {/* ) : ( <a href="/" alt="accueil" ></a>)} */}
+                        {/* ) : (<a href="/" alt="accueil" ></a>)} */}
                     </div>
+
+
+                </span >
+                <div className="search">
+                    <input type="search" placeholder="rechercher" />
+                    <Search />
+                    <button><img className="" src={search} alt=' icone de recherche' /></button>
                     <ToggleBtn />
-                </span>
+                </div>
+
             </header >
         </>
 
