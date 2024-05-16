a62664f67e2cfd3a8696c01016764ad32c1212b3
Camille-Chartrain <camille.chartrain@oclock.school>
2024-05-16 16:00:39 +0200

    doing road searchVisitor

--- BACK/app/controllers/homeController.js
+++ BACK/app/controllers/homeController.js
@@ -32,6 +32,34 @@ const homeController = {
             res.render('error');
         }
     },
+
+    searchVisitor: async function (req, res) {
+        try {
+            // req.params contain            const search = req.params;
+            const skill = await Skill.findAll({
+                order: [['id', 'DESC']], // order by descent with id
+                limit: 4, // Limit to 4 results
+                include: [{
+                    model: User, // Table to join
+                    attributes: ['firstname', 'lastname', "email", 'grade_level', "presentation"] // Seclect specified attributs of the table Commande
+                }, {
+                    model: Category,
+                    attributes: ['picture', 'name']
+                },
+                {
+                    model: Sub_category,
+                    attributes: ['name']
+                }],
+            });
+            //send the answer to the front
+            res.send(
+                skill
+            );
+        } catch (error) {
+            console.error(error.message);
+            res.render('error');
+        }
+    },
 };
 
 export default homeController;
