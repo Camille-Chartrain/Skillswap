6967fd15411914529cbd58a5a73326d7e8d8a700
Camille-Chartrain <camille.chartrain@oclock.school>
2024-05-27 16:10:08 +0200

    CC- middleware verifyToken ok

--- BACK/app/middlewares.js/verifyToken.js
+++ BACK/app/middlewares.js/verifyToken.js
@@ -7,50 +7,54 @@ function verifyToken(req, res, next) {
     // console.log("req.headers['authorization']:", req.headers);
     const token = authHeader && authHeader.split(' ')[1]
 
-    // try {
-    //     if (token == null) {
-    //         throw new Error('Token non fourni')
-    //     }
-    //     if (token) {
-    //         jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    //             console.log('dans la fonction verifytoken');
-    //             if (err) {
-    //                 throw new Error('Token invalide')
-    //             }
-    //             req.user = user;
-    //             console.log("req.user", req.user);
-    //         });
-    //     }
-    // }
-    // catch (error) {
-    //     res.send(error);
-    // }
-
-
-    if (token == null) {
-        return (
-            res.status(403).json({ message: 'Token non fourni' })
-        );
-    }
-    if (token) {
-        try {
+    try {
+        if (token == null) {
+            console.log("check s'il y a token");
+            throw new Error('Token non fourni')
+        }
+        if (token) {
             jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-                if (err) return (
-                    res.status(403).json({ message: 'Token invalide' })
-                )
+                console.log('dans la fonction verifytoken');
+                if (err) {
+                    throw new Error('Token invalide')
+                }
                 req.user = user;
                 console.log("req.user", req.user);
-            })
-        } catch (error) {
-            console.log("error:", error);
-            // res.status(401).json({ message: 'Token invalide' });
+                console.log('on est apres le verify, token validé');
+
+                next();
+            });
         }
     }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 
-    console.log('on est apres le verify=================================');
-    //req.userId = decodedToken.user.id;
+    // if (token == null) {
+    //     console.log("check s'il y a token");
+    //     return (
+    //         res.status(403).json({ message: 'Token non fourni' })
+    //     );
+    // }
+    // if (token) {
+    //     try {
+    //         jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    //             console.log('dans la fonction verifytoken');
+    //             if (err) return (
+    //                 res.status(403).json({ message: 'Token invalide' })
+    //             )
+    //             req.user = user;
+    //             console.log("req.user", req.user);
+    //         })
+    //     } catch (error) {
+    //         console.log("error:", error);
+    //         // res.status(401).json({ message: 'Token invalide' });
+    //     }
+    // }
+    // console.log('on est apres le verify=================================');
+    // req.userId = decodedToken.user.id;
     // console.log("decoded token:", decodedToken);
-    next();
+    // next();
 };
 
 
