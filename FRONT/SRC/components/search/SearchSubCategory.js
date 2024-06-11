76576e8ffbf2c4356947613b1006b05fea36e963
lucilebgt <lucile.bergeat@oclock.school>
2024-06-11 07:57:28 +0200

    search in progress

--- FRONT/SRC/components/search/SearchSubCategory.js
+++ FRONT/SRC/components/search/SearchSubCategory.js
@@ -1,85 +1,88 @@
 import { useState, useEffect, useCallback } from "react";
 import { useForm } from "react-hook-form";
 import Cookies from 'js-cookie';
+import { useLocation } from "react-router-dom";
 
 
-const SearchSubCategory = (selectCat) => {
+const SearchSubCategory = (setValue) => {
     const { register, handleSubmit } = useForm();
 
+    const location = useLocation();
+    const selectCat = location.state?.selectCat;
+
     //= to fetch select's datas and datas bdd
-    const [selectSubCat, setSelectSubCat] = useState([selectCat||{
-        id: [],
-        name: '',
-        Category_id: [],
-    }]);
-    console.log("try recup selecSubCat id ", selectSubCat.id);
+    const [selectSubCat, setSelectSubCat] = useState([
+    ]);
+
+
+    console.log("try recup selecSubCat ", selectSubCat);
     console.log("try recup selecCat ", selectCat);
 
     const handleChangeSubCat = (e) => {
         const { name, value } = e.target;
         console.log('handleChange: ', name, value);
-        setSelectSubCat((prevSelectSubCat) => ({
-            ...prevSelectSubCat,
+        setSelectSubCat((prevSelectCat) => ({
+            ...prevSelectCat,
             [name]: value,
         }));
-        // setValue(name, value);
+        setValue(name, value);
     };
 
+    useEffect(() => {
+        if (selectCat) {
+            getSubCategoriesList();
+        }
+    }, [selectCat]);
 
-    const getSubCategoriesList = async ( selectCat) => {
-        console.log("selectCat id avant try:", selectCat);
+
+    const getSubCategoriesList = async () => {
+        console.log("selectCat avant try:", selectCat);
         try {
             const token = Cookies.get('token');
             const response = await fetch(`http://localhost:3000/subCategories/${selectCat.id}`, {
                 method: "get",
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${ token }`,
+                    'Authorization': `Bearer ${token}`,
                 },
                 // credentials: 'include'
-            });       
+            });
             const dataSubCategories = await response.json();
             console.log("dataSubCategories avant JSON: ", dataSubCategories);
             setSelectSubCat(dataSubCategories);
-            console.log("dataSubCategories apres JSON: ",dataSubCategories);
+            console.log("dataSubCategories apres JSON: ", dataSubCategories);
+
+            //= update recup's values
+            Object.keys(dataSubCategories).forEach(key => {
+                setValue(key, dataSubCategories[key]);
+            });
+
         }
         catch (error) {
-            console.error("catch GetSubCategoriesList:", error.message);
+            console.error("catch GetSubCategoriesList:", error);
         }
     };
 
-    useEffect(() => { getSubCategoriesList()}, []);
 
     return (
-            <select id="SubCategoryId" name="SubCategoryId" onSubmit={handleSubmit(getSubCategoriesList.bind(null,selectSubCat))}>
-                <option value="">Choisissez votre sous-categorie</option>
-                
-                {selectSubCat.map((subCategory) => {                   
-                    <option key={subCategory.id} value={subCategory.id}{...register("subCategory.id")} name={"subCategory.id"} onChange={handleChangeSubCat}> {subCategory.name}</option>   
-                })}
-                
-            </select>
+        <>
+            {selectCat === true ? (
+                <select id="SubCategoryId" name="SubCategoryId" onChange={handleChangeSubCat.bind(null, setSelectSubCat)}>
+                    <option value=""  >choisissez votre sous-categorie</option>
+                    {
+                        selectSubCat.map((selectSubCat) => (
+                            <option key={selectSubCat.id} value={selectSubCat.id}>{selectSubCat.name}</option>
+                        ))
+                    }
+                </select >
+
+            ) : (
+                <option value="all"> {selectSubCat.name}</option>
+            )
+            }
+        </>
     )
 
 };
 export default SearchSubCategory;
 
-
-// //=spag
-// <select id="SubCategoryId" name="SubCategoryId" onSubmit={handleSubmit(getSubCategoriesList)}>
-//     <option value=""  >choisissez votre sous-categorie</option>
-//     {selectCat.map((category) => (
-//         <option id={category.id} key={category.id} value={category.id} {...register("category.id")} name={"category.id"} onChange={handleChangeCat}> {category.name}</option>
-                    
-//                     {
-//             selectSubCat.map((selectSubCat) => (
-//                 <option key={selectSubCat.id} value={selectSubCat.id}>{selectSubCat.name}</option>
-//             ))
-//         }
-//     ))}
-
-//     ) : (
-//     <option value="all"> {selectSubCat.name}</option>
-//     )
-//                 }
-// </select >
